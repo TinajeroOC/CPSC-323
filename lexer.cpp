e1248c7de298fa7af45742776ac37ef99576c8a8
AustinHNguyen <123607182+AustinHNguyen@users.noreply.github.com>
2023-02-13 23:23:58 -0800

    make tokenizer
    I stashed, but it didn't give me merge prompt so I just pushed anyway

--- lexer.cpp
+++ lexer.cpp
@@ -10,22 +10,50 @@ Lexer::~Lexer() {
 
 void Lexer::tokenize(std::ifstream &input_file) {
     bool isComment = false;
-    std::string token;
+    std::string word;
     std::string nextLine;
+    std::string tokenHolder;
+    int wordSize = 0;
     while (std::getline(input_file, nextLine))
     {
         std::stringstream checker(nextLine);
-        while (checker >> token)
+        while (checker >> word)
         {
-            if (token.find("[*") == std::string::npos && isComment != true)
+            if (word.find("[*") == std::string::npos && isComment != true)
             {
-                std::cout << token;
-                std::cout << std::endl;
+                wordSize = word.size();
+                for (int i = 0; i < wordSize; i++)
+                {
+                    if (is_separator(std::string(1, word[i])))
+                    {
+                        std::cout << "Separator: " << std::string(1, word[i]) << std::endl;
+                        Token token = {SEPARATOR, std::string(1, word[i])};
+                    }
+                    else
+                    {
+                        tokenHolder += std::string(1, word[i]);
+                        if (i == wordSize-1)
+                        {
+                            if (is_operator(std::string(tokenHolder)))
+                            {
+                                std::cout << "Operator: " << tokenHolder << std::endl;
+                                Token token = {SEPARATOR, tokenHolder};
+                            }
+                            if (is_keyword(std::string(tokenHolder)))
+                            {
+                                std::cout << "Keyword: " << tokenHolder << std::endl;
+                                Token token = {KEYWORD, tokenHolder};
+                            }
+                            tokenHolder = "";
+                        }
+                    }
+                }
+                tokenHolder = "";
             }
             else
             {
                 isComment = true;
-                if (token.find("*]") != std::string::npos)
+                if (word.find("*]") != std::string::npos)
                 {
                     isComment = false;
                 }
@@ -35,17 +63,7 @@ void Lexer::tokenize(std::ifstream &input_file) {
 }
 
 void Lexer::results(std::ofstream &output_file) {
-    std::string tokenType;
-    #define underline "\033[4m"
-    std::cout << underline << "Output:";
-    #define underline "\033[0m"
-    std::cout << "\ntoken";
-    std::cout << std::setw(15) << "lexeme" << std::endl;
-    for (int i = 0; i < tokens.size(); i++)
-    {
-        std::cout << std::setw(15);
-    }
-
+    std::string test = "Test";
 }
 
 bool Lexer::is_identifier(const std::string &lexeme) {
